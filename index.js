df082ddfee82e6fa0d857bc6b8b4ddd96a52f1bd
Tomasz Rakowski <tomasz.rakowski@jomediainc.com>
2015-11-30 16:43:03 -0500

    First version

---
+++ index.js
@@ -0,0 +1,59 @@
+var merge = require('merge');
+
+module.exports = function(option) {
+
+	function backend(option) {
+
+		option = merge(true, {depth: 1000}, option);
+
+		var stack = [];	// Keeps track of access order and length
+		var store = {}; // Maps quickly to data
+
+		this.set = function(key, value, options, cb) {
+			store[key] = {
+				v: value,
+				timeout: setTimeout(function() {
+					delete store[key];
+				}, options.ttl)
+			};
+			stack.unshift(key);
+			// Callback and resize are inversed on purpose to improve speed.
+			cb();
+			resize();
+		};
+
+		this.get = function(key, options, cb) {
+			if (store[key]) {
+				cb(null, store[key].v);
+				stack.unshift(stack.pop());
+			} else {
+				cb(new Error('Key not found'));
+			}
+		};
+
+		this.delete = function(key, options, cb) {
+			remove(key);
+			cb();
+		};
+
+		this.depth = function() {
+			return stack.length;
+		}
+
+		function remove(key) {
+			if (store[key]) {
+				clearTimeout(store[key].timeout);
+				delete store[key];
+			}
+		};
+
+		function resize() {
+			var i = stack.length - 1;
+			while(i >= option.depth) {
+				remove(stack.pop());
+				i--;
+			}
+		}
+	}
+	return new backend(option);
+};
---
+++ test/main.js
@@ -0,0 +1,150 @@
+var cc = require('cache-chain');
+var async = require('async');
+
+describe('Main Tests for cache-chain-memory', function() {
+
+	var chain;
+
+	beforeEach(function() {
+		chain = cc.chain({
+			ttl: 10000,
+			stale: 10000 * 2
+		});
+		var layer = cc.layer(require('../index.js')({
+			depth: 1000
+		}));
+		chain.append(layer);
+	});
+
+	it('It needs to set a value', function(done) {
+		var key = 'key';
+		var value = 'value';
+		chain.set(key, value, function(err, reply) {
+			if (err) {
+				done(err);
+			} else {
+				chain.get(key, function(err, reply) {
+					if (reply === value) {
+						done();
+					} else {
+						done(err);
+					}
+				});
+			}
+		})
+	});
+
+	it('It needs to retrive a set value', function(done) {
+		var key = 'key';
+		var value = 'value';
+		chain.set(key, value, function(err, reply) {
+			if (err) {
+				done(err);
+			} else {
+				chain.get(key, function(err, reply) {
+					if (err) {
+						done(err);
+					} else {
+						if (reply == value) {
+							done();
+						} else {
+							done(reply)
+						}
+					}
+				})
+			}
+		})
+	});
+
+	it('It needs to delete a set value', function(done) {
+		var key = 'key';
+		var value = 'value';
+		chain.set(key, value, function(err, reply) {
+			if (err) {
+				done(err);
+			} else {
+				chain.delete(key, function(err) {
+					if (err) {
+						cb(err);
+					} else {
+						chain.get(key, function(err, reply) {
+							if (err) {
+								if (err.message == "Key not found") {
+									done();
+								} else {
+									done('Wrong error message returned');
+								}
+							} else {
+								done('Expected error message. Got correct reply instead')
+							}
+						})
+					}
+				})
+			}
+		})
+	});
+
+	it('A value needs to timeout', function(done) {
+		var key = 'key';
+		var value = 'value';
+		chain.set(key, value, {
+			ttl: 10,
+			stale: 5
+		}, function(err, reply) {
+			if (err) {
+				done(err);
+			} else {
+				setTimeout(function() {
+					chain.get(key, function(err, reply) {
+						if (err) {
+							if (err.message == "Key not found") {
+								done();
+							} else {
+								done('Wrong error message returned');
+							}
+						} else {
+							done('Expected error message. Got correct reply instead')
+						}
+					})
+				}, 15);
+			}
+		})
+	});
+
+	it('Has to respect memory store depth', function(done) {
+		var key = 'key';
+		var value = 'value';
+		var d = 3;
+		chain = cc.chain({
+			ttl: 10000,
+			stale: 10000 * 2
+		});
+		var backend = require('../index.js')({
+			depth: d
+		});
+		var layer = cc.layer(backend);
+		chain.append(layer);
+		var set = [];
+		var i = 0;
+		while (i < 5) {
+			set.push(function(callback) {
+				chain.set('key' + this.i, 'value', function(err, result) {
+					callback(null, err);
+				})
+			}.bind({i: i}))
+			i++;
+		}
+		async.series(
+			set,
+			function(err, results) {
+				setTimeout(function() {
+					if(backend.depth() == d) {
+						done();
+					} else {
+						done('Memory store depth has returned unexpected results');
+					}
+				}, 5);
+			}
+		)
+	});
+});
