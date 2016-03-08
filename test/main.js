7736c09e7d73e0839c9fc906487e3e9c8b30d34f
Tomasz Rakowski <tomasz.rakowski@jomediainc.com>
2016-03-08 13:39:11 -0500

    Modified to use centralized intergration tests

--- test/main.js
+++ test/main.js
@@ -3,148 +3,63 @@ var async = require('async');
 
 describe('Main Tests for cache-chain-memory', function() {
 
+	var di = {
+		chain: null,
+		cc: require('../../cache-chain')
+	}
+
 	var chain;
 
-	beforeEach(function() {
-		chain = cc.chain({
+	beforeEach(function(done) {
+		di.chain = di.cc.chain({
 			ttl: 10000,
 			stale: 10000 * 2
 		});
-		var layer = cc.layer(require('../index.js')({
+		var layer = di.cc.layer(require('../index.js')({
 			depth: 1000
 		}));
-		chain.append(layer);
+		di.chain.append(layer);
+		done();
 	});
 
-	it('It needs to set a value', function(done) {
-		var key = 'key';
-		var value = 'value';
-		chain.set(key, value, function(err, reply) {
-			if (err) {
-				done(err);
-			} else {
-				chain.get(key, function(err, reply) {
-					if (reply === value) {
-						done();
-					} else {
-						done(err);
-					}
-				});
+	require('../../cache-chain/test/integration')(di);
+
+	describe('Memcached Specific Tests', function() {
+		it('Has to respect memory store depth', function(done) {
+			var key = 'key';
+			var value = 'value';
+			var d = 3;
+			chain = di.cc.chain({
+				ttl: 10000,
+				stale: 10000 * 2
+			});
+			var backend = require('../index.js')({
+				depth: d
+			});
+			var layer = di.cc.layer(backend);
+			chain.append(layer);
+			var set = [];
+			var i = 0;
+			while (i < 5) {
+				set.push(function(callback) {
+					chain.set('key' + this.i, 'value', function(err, result) {
+						callback(null, err);
+					})
+				}.bind({i: i}))
+				i++;
 			}
-		})
-	});
-
-	it('It needs to retrive a set value', function(done) {
-		var key = 'key';
-		var value = 'value';
-		chain.set(key, value, function(err, reply) {
-			if (err) {
-				done(err);
-			} else {
-				chain.get(key, function(err, reply) {
-					if (err) {
-						done(err);
-					} else {
-						if (reply == value) {
+			async.series(
+				set,
+				function(err, results) {
+					setTimeout(function() {
+						if(backend.depth() == d) {
 							done();
 						} else {
-							done(reply)
+							done('Memory store depth has returned unexpected results');
 						}
-					}
-				})
-			}
-		})
-	});
-
-	it('It needs to delete a set value', function(done) {
-		var key = 'key';
-		var value = 'value';
-		chain.set(key, value, function(err, reply) {
-			if (err) {
-				done(err);
-			} else {
-				chain.delete(key, function(err) {
-					if (err) {
-						cb(err);
-					} else {
-						chain.get(key, function(err, reply) {
-							if (err) {
-								if(err instanceof cc.error.notFound) {
-									done();
-								} else {
-									done('Wrong error message returned');
-								}
-							} else {
-								done('Expected error message. Got correct reply instead')
-							}
-						})
-					}
-				})
-			}
-		})
-	});
-
-	it('A value needs to timeout', function(done) {
-		var key = 'key';
-		var value = 'value';
-		chain.set(key, value, {
-			ttl: 10,
-			stale: 5
-		}, function(err, reply) {
-			if (err) {
-				done(err);
-			} else {
-				setTimeout(function() {
-					chain.get(key, function(err, reply) {
-						if (err) {
-							if(err instanceof cc.error.notFound) {
-								done();
-							} else {
-								done('Wrong error message returned');
-							}
-						} else {
-							done('Expected error message. Got correct reply instead')
-						}
-					})
-				}, 15);
-			}
-		})
-	});
-
-	it('Has to respect memory store depth', function(done) {
-		var key = 'key';
-		var value = 'value';
-		var d = 3;
-		chain = cc.chain({
-			ttl: 10000,
-			stale: 10000 * 2
+					}, 5);
+				}
+			)
 		});
-		var backend = require('../index.js')({
-			depth: d
-		});
-		var layer = cc.layer(backend);
-		chain.append(layer);
-		var set = [];
-		var i = 0;
-		while (i < 5) {
-			set.push(function(callback) {
-				chain.set('key' + this.i, 'value', function(err, result) {
-					callback(null, err);
-				})
-			}.bind({i: i}))
-			i++;
-		}
-		async.series(
-			set,
-			function(err, results) {
-				setTimeout(function() {
-					if(backend.depth() == d) {
-						done();
-					} else {
-						done('Memory store depth has returned unexpected results');
-					}
-				}, 5);
-			}
-		)
-	});
+	})
 });
